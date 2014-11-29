a04d1995b909b0da65e6e962a6ef60d19095e578
Isaac White <iaw2105@columbia.edu>
2014-11-29 01:15:02 -0500

    Install ohmyzsh manually and provide a zshrc. Puppet ohmyzsh module fails via digitalocean provisioner.

--- puppet/modules/personal/manifests/zsh_config.pp
+++ puppet/modules/personal/manifests/zsh_config.pp
@@ -7,35 +7,54 @@ class personal::zsh_config {
 		$username = $personal::params::box_username
 	} else {
 		$username = 'isaac'
+
+		user {'isaac':
+			ensure => present,
+			password => '$6$4wsq.4tKNDF8wxCQ$3dGv4oWoHZN.o022CgxH2W.Ho2roE3/ggMyI2mR8Bell1esYaFuoruuhsbCZxWLPzgzcnlo5kNfDTDP9hC4De1',
+			managehome => true,
+			shell => '/bin/zsh',
+		}
 	}
+
 	$zsh_theme = $personal::params::zsh_theme
 	$zsh_filename = $personal::params::zsh_theme_filename
 
-	# symlink to make ohmyzsh work.
-	file {'/usr/bin/zsh':
-		ensure => link,
-		target => '/bin/zsh',
+	$user_folder = "/home/${username}/.oh-my-zsh"
+
+	# the ohmyzsh module doesn't seem to work properly,
+	# we'll do the following configs manually.
+
+	if(!defined(Package['git'])) {
+		package { 'git':
+			ensure => present,
+		}
 	}
 
-	# include ohmyzsh module after symlink is setup
-	->
-	class{ 'ohmyzsh':}
-
-	# install for environment user
-	ohmyzsh::install{$username:
+	if(!defined(Package['zsh'])) {
+		package { 'zsh':
+			ensure => present,
+		}
 	}
 
-	# add the custom theme after ohmyzsh is configured
-	->
+	$indicator_file = '/ohmyzsh-installed'
+	exec {'install ohmyzsh':
+		command => "/usr/bin/git clone https://github.com/robbyrussell/oh-my-zsh.git ${user_folder} && touch ${indicator_file}",
+		require => Package['git','zsh'],
+		creates => $indicator_file,
+	}
+
 	file {'custom zsh theme':
 		ensure => present,
 		path => "/home/$username/.oh-my-zsh/themes/$zsh_filename",
 		source => "puppet:///modules/personal/$zsh_filename",
+		require => Exec['install ohmyzsh'],
 	}
 
-	# set the theme after the custom file has been added.
-	->
-	ohmyzsh::theme{$username:
-		theme => $zsh_theme,
+	file {'zshrc':
+		ensure => present,
+		path => "/home/$username/.zshrc",
+		source => "puppet:///modules/personal/.zshrc",
+		require => Exec['install ohmyzsh'],
 	}
+
 }
